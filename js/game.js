1e97fbe7f3392092c13f2762b020cb4c7280f392
FeliksDev <igortaranovua@gmail.com>
2025-07-17 18:17:09 +0300

    fix

--- js/game.js
+++ js/game.js
@@ -1,8 +1,16 @@
 var mymap = L.map('mapid').setView([50.4501, 30.5234], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+function initMap() {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
+}
+function addMarker(pos, tit, desc) {
+    L.marker(pos).addTo(mymap)
+        .bindPopup(`<b>${tit}</b><br>${desc}`); 
+}
 
-L.marker([50.4501, 30.5234]).addTo(mymap)
-    .bindPopup("<b>Ціль</b><br>Тест").openPopup();
+initMap()
+
+
+addMarker([51.09775, 29.59541], "Ціль", "g")
+addMarker([51.252, 36.947], "Ціль", "test" )
